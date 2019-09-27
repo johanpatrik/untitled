e0b83f09f29c6f5e1072f92cd913f3d78a3252ee
Johan Özbek <JohanOzbek@gmail.com>
2019-09-27 11:02:25 +0200

    Jag la till en metod.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hejsan 12345678");
+        System.out.println("En ny sout");
+        print();
+    }
+
+    public static void print(){
         System.out.println("Hejsan");
     }
 }
